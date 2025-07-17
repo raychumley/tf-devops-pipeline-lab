243eab4c73cbecec1d19bca44040d4fcb259e1f6
raychumley <ray.chumley@watech.wa.gov>
2025-07-17 23:04:28 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   subscription_id = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc"
 }
 
-resource "azurerm_resource_group" "rg2" {
+resource "azurerm_resource_group" "rg" {
   name      = "rayc-basic-pipeline-test2"
   location  = "westus2"
 }
