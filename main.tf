5b44cf7c7a122a7932bafca00aa22806c9589439
raychumley <ray.chumley@watech.wa.gov>
2025-07-17 22:06:21 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rayc-state-rg-pipeline"
+    storage_account_name = "raycstatesapipeline"
+    container_name       = "tfstate"         # 👈 This is the container (like a folder)
+    key                  = "terraform.tfstate" # 👈 This is the blob (file)
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc"
